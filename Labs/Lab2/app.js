ac000def212abe6c423231cfa4fa29543d5c69c4
epapostolou <77795638+epapostolou@users.noreply.github.com>
2022-01-18 22:54:32 -0500

    added variables to personal composition

--- Labs/Lab2/app.js
+++ Labs/Lab2/app.js
@@ -1,7 +1,8 @@
 //Evan Apostolou
 //1/18/2022
 //n220
-
+var eyes = 30
+var smile = 50
 function setup() {
     createCanvas(800, 600);
     //make base face (circle with skin tone)
@@ -9,13 +10,13 @@ function setup() {
     circle(400, 300, 300);
     //draw eyes
     fill(255, 255, 255);
-    circle(350, 250, 30);
-    circle(450, 250, 30);
+    circle(350, 250, eyes);
+    circle(450, 250, eyes);
     //draw lips
     stroke(117, 21, 52);
     strokeWeight(25);
     fill(212, 167, 108);
-    arc(400, 350, 50, 50, 0, PI);
+    arc(400, 350, smile, smile, 0, PI);
 }
 
 
